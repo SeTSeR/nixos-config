9738699bba40995c3601a300e980157e1ccbd002
Sergey Makarov <setser200018@gmail.com>
2019-02-15 22:21:00 +0300

    Fix installation script

--- install.sh
+++ install.sh
@@ -2,24 +2,26 @@
 
 echo "SeTSeR's config installation."
 
-nix-shell -p "git" --run "git submodule update --init --recursive"
+nix-env -iA nixos.git
+git submodule update --init --recursive
 
 CONFIG="/mnt/etc/nixos/configuration.nix"
 USER_HOME="/home/smakarov"
 
 echo "Creating ${CONFIG}"
 mkdir -p /mnt/etc/nixos
-echo "{config, pkgs, ...}:
+echo "{ config, pkgs, options, ... }:
 {
     imports = [
         ./hardware-configuration.nix
         $(pwd)
     ];
-}" > $(CONFIG)
+}" > $CONFIG
 echo "Done"
 echo "Installing"
 nixos-generate-config --root /mnt
 nixos-install
+mkdir /mnt/$USER_HOME/.config
 ln -s $(pwd)/.config/i3status /mnt/$USER_HOME/.config/i3status
 ln -s $(pwd)/.config/nixpkgs /mnt/$USER_HOME/.config/nixpkgs
 echo "Done"
