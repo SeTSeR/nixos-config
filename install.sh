b3b970f09729ab173bff4f6d840f1638d234eaac
Sergey Makarov <setser200018@gmail.com>
2019-02-16 18:04:52 +0300

    Clean installation script

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 echo "SeTSeR's config installation."
 
 nix-env -iA nixos.git
-git submodule update --init --recursive
 
 CONFIG="/mnt/etc/nixos/configuration.nix"
 USER_HOME="/home/smakarov"
@@ -17,16 +16,11 @@ echo "Installing"
 nixos-generate-config --root /mnt
 ln -s /mnt/etc/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 nixos-install
+rm /etc/nixos/hardware-configuration.nix
+nix-env -e nixos.git
 if [ $? -eq 0 ]; then
-    mkdir /mnt/$USER_HOME/.config
-    ln -s .config/i3status /mnt/$USER_HOME/.config/i3status
-    ln -s .config/nixpkgs /mnt/$USER_HOME/.config/nixpkgs
-    rm /etc/nixos/hardware-configuration.nix
-    nix-env -e nixos.git
     echo "Done"
 else
-    rm /etc/nixos/hardware-configuration.nix
     rm -r /mnt/etc/nixos
-    nix-env -e nixos.git
     echo "Installation failed"
 fi
