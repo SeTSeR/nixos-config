0d477de4b3aacce39e06d41f24c2d9325ee1551a
Sergey Makarov <setser200018@gmail.com>
2019-02-17 20:13:29 +0300

    Add hotkeys to change keyboard brightness

--- modules/users/smakarov/i3.nix
+++ modules/users/smakarov/i3.nix
@@ -71,6 +71,8 @@ in
         "XF86AudioMute" = "exec --no-startup-id ${pkgs.pulseaudio}/bin/pactl set-sink-mute 0 toggle";
         "XF86MonBrightnessUp" = "exec ${pkgs.acpilight}/bin/xbacklight -ctrl intel_backlight -inc 20";
         "XF86MonBrightnessDown" = "exec ${pkgs.acpilight}/bin/xbacklight -ctrl intel_backlight -dec 20";
+        "XF86KbdBrightnessUp" = "exec ${pkgs.acpilight}/bin/xbacklight -ctrl asus::kbd_backlight -inc 20";
+        "XF86KbdBrightnessDown" = "exec ${pkgs.acpilight}/bin/xbacklight -ctrl asus::kbd_backlight -dec 20";
         "--release Print" = "exec --no-startup-id \"import png:- | xclip -selection c -t image/png\"";
       } // builtins.listToAttrs (
         builtins.genList (x: {name = "${modifier}+${toString (x + 6)}"; value = "workspace ${toString (x + 6)}";}) 4
