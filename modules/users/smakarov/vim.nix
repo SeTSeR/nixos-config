098c8a3253a8e8337958597919e234e4d93a8931
Sergey Makarov <setser200018@gmail.com>
2019-02-28 19:35:35 +0000

    Update vim config

--- modules/users/smakarov/vim.nix
+++ modules/users/smakarov/vim.nix
@@ -52,6 +52,13 @@ set shiftwidth=4
 set softtabstop=4
 set expandtab
 
+let g:netrw_browse_split = 4
+let g:netrw_winsize = 25
+augroup ProjectDrawer
+  au!
+  au VimEnter * :Vexplore
+augroup END
+
 filetype plugin indent on
 syntax enable
 colorscheme solarized
