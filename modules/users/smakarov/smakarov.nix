73be0344cc3370bdd40788c91fef034d7def0223
Sergey Makarov <setser200018@gmail.com>
2019-03-07 11:58:53 +0300

    Add adb

--- modules/users/smakarov/smakarov.nix
+++ modules/users/smakarov/smakarov.nix
@@ -2,12 +2,13 @@
 {
 
   programs.zsh.enable = true;
+  programs.adb.enable = true;
   # Set up the user
   users.users.smakarov = {
     isNormalUser = true;
     home = "/home/smakarov";
     description = "Sergey Makarov";
-    extraGroups = [ "wheel" "networkmanager" "video" "libvirt" ];
+    extraGroups = [ "wheel" "networkmanager" "video" "libvirt" "adbusers" ];
     hashedPassword = "$6$bhfILKl6NKxZT25$wOQ0A9AtNYLCGLHcR4Bee7VBzYUusq4Af.DAL4Qr5c12JN3LBYH1PFtm.UvCcvXjZ1PbpuhGndnQCgbPaj/.C.";
     shell = pkgs.zsh;
   };
