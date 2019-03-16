5fbd75d687bd812c19525838128903abf4218488
Sergey Makarov <setser200018@gmail.com>
2019-03-16 18:11:00 +0300

    Add device-specific options

--- modules/devices.nix
+++ modules/devices.nix
@@ -4,5 +4,13 @@ with types;
 {
   options = {
     device = mkOption { type = strMatching "[A-Za-z]*\-[A-Za-z]*"; };
+    deviceSpecific = mkOption { type = attrs; };
+  };
+  
+  config = {
+    deviceSpecific = let device = config.device;
+    in rec {
+      isHomeMachine = (device == "ASUS-Laptop");
+    };
   };
 }
