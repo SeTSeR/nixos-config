b278bd578810735fdb8ee11865136a5c0bb810d4
Sergey Makarov <setser200018@gmail.com>
2019-03-24 20:43:33 +0300

    Remove adb remains

--- modules/users/smakarov/smakarov.nix
+++ modules/users/smakarov/smakarov.nix
@@ -8,7 +8,7 @@
     isNormalUser = true;
     home = "/home/smakarov";
     description = "Sergey Makarov";
-    extraGroups = [ "wheel" "networkmanager" "video" "libvirt" "adbusers" ];
+    extraGroups = [ "wheel" "networkmanager" "video" "libvirt" ];
     hashedPassword = "$6$bhfILKl6NKxZT25$wOQ0A9AtNYLCGLHcR4Bee7VBzYUusq4Af.DAL4Qr5c12JN3LBYH1PFtm.UvCcvXjZ1PbpuhGndnQCgbPaj/.C.";
     shell = pkgs.zsh;
   };
