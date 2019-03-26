9c60e7f6cc133174620cafa3ab2a0f4bc0da26eb
Sergey Makarov <setser200018@gmail.com>
2019-03-26 20:22:58 +0300

    Disable redshift

--- modules/users/smakarov/services.nix
+++ modules/users/smakarov/services.nix
@@ -1,13 +1,6 @@
 { config, pkgs, ... }:
 {
   home-manager.users.smakarov = {
-    services.redshift = {
-      enable = true;
-      brightness.day = "1";
-      brightness.night = "0.7";
-      provider = "geoclue2";
-      temperature.night = 1588;
-    };
     services.blueman-applet.enable = true;
     services.udiskie = {
       enable = true;
