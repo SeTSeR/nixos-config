b0b2f21ccb3b02a8baf60864c8b1f541627b0b41
Sergey Makarov <setser200018@gmail.com>
2019-03-28 20:41:49 +0300

    Add users mutability on non-home machines

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,10 +1,10 @@
-{pkgs, ...}:
+{pkgs, config, ...}:
 {
   imports = [
     ./smakarov
   ];
 
-  users.mutableUsers = false;
+  users.mutableUsers = !config.deviceSpecific.isHomeMachine;
   users.users.root.hashedPassword = "$6$I1DA5i2qtMUiZe$lhEzV/.ikRj.hRDBdUVN391VPCxsZWMUBgb.aKySTMa43VE2JmD/F8CvvgPy1cgesA7gEg9eFum6S9wxHxnu9.";
 
   security.sudo = {
