06c7e5ccaa75e84c0fd53330c648b83f70239e92
Sergey Makarov <setser200018@gmail.com>
2019-03-29 17:27:14 +0300

    Revert "Add users mutability on non-home machines"
    This reverts commit b0b2f21ccb3b02a8baf60864c8b1f541627b0b41.

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,10 +1,10 @@
-{pkgs, config, ...}:
+{pkgs, ...}:
 {
   imports = [
     ./smakarov
   ];
 
-  users.mutableUsers = !config.deviceSpecific.isHomeMachine;
+  users.mutableUsers = false;
   users.users.root.hashedPassword = "$6$I1DA5i2qtMUiZe$lhEzV/.ikRj.hRDBdUVN391VPCxsZWMUBgb.aKySTMa43VE2JmD/F8CvvgPy1cgesA7gEg9eFum6S9wxHxnu9.";
 
   security.sudo = {
