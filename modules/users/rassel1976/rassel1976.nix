ec0fe0f3ab72c94f157a8c78074bcad13f997f38
Sergey Makarov <setser200018@gmail.com>
2019-04-04 22:38:25 +0300

    Add password for rassel1976

--- modules/users/rassel1976/rassel1976.nix
+++ modules/users/rassel1976/rassel1976.nix
@@ -6,7 +6,7 @@
     home = "/home/rassel1976";
     description = "Nikita Golovanov";
     extraGroups = [ "networkmanager" ];
-    password = "";
+    hashedPassword = "BF985284FFDD3429B10EA7F6422FE1D248A5D4390DA37DA7A103CA54F8E739CE00B7EF1A6F8C579B281D34E4D22B908C72899399558D639A0CF85ED7260CE569";
   } else lib.mkForce {};
 
   systemd.services.home-manager-rassel1976.enable = if config.deviceSpecific.isWorkMachine then true
