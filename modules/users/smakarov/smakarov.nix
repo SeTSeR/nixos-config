27f3bc22a6e2e21d411d433fd2e4693e514b857a
Sergey Makarov <setser200018@gmail.com>
2019-04-24 00:42:30 +0300

    Update groups for smakarov

--- modules/users/smakarov/smakarov.nix
+++ modules/users/smakarov/smakarov.nix
@@ -7,7 +7,7 @@
     isNormalUser = true;
     home = "/home/smakarov";
     description = "Sergey Makarov";
-    extraGroups = [ "wheel" "networkmanager" "video" "libvirt" ];
+    extraGroups = [ "wheel" "networkmanager" "video" "storage" ];
     hashedPassword = "$6$bhfILKl6NKxZT25$wOQ0A9AtNYLCGLHcR4Bee7VBzYUusq4Af.DAL4Qr5c12JN3LBYH1PFtm.UvCcvXjZ1PbpuhGndnQCgbPaj/.C.";
     shell = pkgs.zsh;
   };
