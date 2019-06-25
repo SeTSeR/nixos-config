c17c93120bd53e52bb0a39cd5d01d9010235c2e8
Sergey Makarov <setser200018@gmail.com>
2019-06-25 14:02:45 +0300

    Reorder file list

--- modules/users/smakarov/emacs/default.nix
+++ modules/users/smakarov/emacs/default.nix
@@ -49,13 +49,13 @@
       ];
     };
     home.file.".emacs.d/cpp.el".source = ./cpp.el;
-    home.file.".emacs.d/lldb-gud.el".source = ./lldb-gud.el;
     home.file.".emacs.d/init.el".source = ./init.el;
     home.file.".emacs.d/keys.el".source = ./keys.el;
+    home.file.".emacs.d/lldb-gud.el".source = ./lldb-gud.el;
+    home.file.".emacs.d/lsp.el".source = ./lsp.el;
     home.file.".emacs.d/org.el".source = ./org.el;
     home.file.".emacs.d/projectile.el".source = ./projectile.el;
     home.file.".emacs.d/rust.el".source = ./rust.el;
-    home.file.".emacs.d/lsp.el".source = ./lsp.el;
     home.file.".emacs.d/spell.el".source = ./spell.el;
     home.file.".emacs.d/yasnippet-snippets".source = ./yasnippet-snippets;
   };
