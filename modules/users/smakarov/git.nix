418aa54e23ac14d033fb227ca97861c398757b63
Sergey Makarov <setser200018@gmail.com>
2019-07-05 14:19:16 +0300

    Update signing key

--- modules/users/smakarov/git.nix
+++ modules/users/smakarov/git.nix
@@ -2,10 +2,10 @@
   home-manager.users.smakarov.programs.git = {
     enable = true;
     signing = {
-      key = "6F8A18AE4101103F3C5424B96AA23A1193B7064B";
+      key = "3C0A077647B0C401F7821DE61CAD6E80CE0210A7";
       signByDefault = true;
     };
-    userEmail = "setser200018@gmail.com";
+    userEmail = "smakarov@ispras.ru";
     userName = "Sergey Makarov";
   };
 }
