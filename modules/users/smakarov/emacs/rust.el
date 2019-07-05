202b171ee715f983ddc91242cc5ea00ce2d477c6
Sergey Makarov <setser200018@gmail.com>
2019-07-05 14:22:17 +0300

    /nix/store/4w42ym67j79l4j1svjmn4szjkp5cbgsk-rust-src fixes

--- modules/users/smakarov/emacs/rust.el
+++ modules/users/smakarov/emacs/rust.el
@@ -10,12 +10,9 @@
   (use-package flycheck-rust
     :hook (flycheck-mode . flycheck-rust-setup))
   (use-package racer
-    :requires rust-mode
     :init
-    (setq racer-rust-src-path
-          (concat (string-trim
-                   (shell-command-to-string "rustc --print systroot"))
-                  "/lib/rustlib/src/rust/src"))
+    (setq racer-rust-src-path nil)
+    :requires rust-mode
     :hook
     (rust-mode . racer-mode)
     (racer-mode . company-mode)
