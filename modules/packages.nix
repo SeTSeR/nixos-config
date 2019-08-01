7815184f8c1ac4e298a3582f3e43f3e762e4eb18
Sergey Makarov <setser200018@gmail.com>
2019-08-01 21:44:14 +0300

    Use Nix from stable branch

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,8 +1,22 @@
-{ config, pkgs, lib, ... }: {
-nixpkgs.overlays = [ (import ./overlay.nix) ];
+{ config, pkgs, lib, ... }:
+let nixpkgs-tars = "https://github.com/NixOS/nixpkgs/archive/";
+in {
+  nixpkgs.overlays = [ (import ./overlay.nix) ];
 
-  nixpkgs.pkgs = import ../imports/nixpkgs { config.allowUnfree = true; }
-  // config.nixpkgs.config;
+  nixpkgs.pkgs = import ../imports/nixpkgs {
+    config = {
+      allowUnfree = true;
+
+      packageOverrides = pkgs: {
+        stable = import (pkgs.fetchzip {
+          url = "${nixpkgs-tars}19.03.zip";
+          sha256 = "0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
+        }) {
+          config = config.nixpkgs.config;
+        };
+      };
+    } // config.nixpkgs.config;
+  };
 
   nix = {
     nixPath = lib.mkForce [
@@ -10,6 +24,8 @@ nixpkgs.overlays = [ (import ./overlay.nix) ];
       "home-manager=${../imports/home-manager}"
       "nixos-config=/etc/nixos/configuration.nix"
     ];
+
+    package = pkgs.stable.nixStable;
   };
 
   # List packages installed in system profile. To search, run:
