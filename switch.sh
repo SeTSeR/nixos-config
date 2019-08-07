9a44530fd2e186b43e787ad3e4837ac83e4b0c1b
Sergey Makarov <setser200018@gmail.com>
2019-08-07 13:16:23 +0300

    Allow to pass --show-trace to switch script

--- switch.sh
+++ switch.sh
@@ -6,7 +6,7 @@ unset IN_NIX_SHELL
 export NIX_PATH=nixpkgs=./imports/nixpkgs:nixos-config=/etc/nixos/configuration.nix
 export SHELL=/bin/sh
 
-nix build -f ./imports/nixpkgs/nixos system && {
+nix build -f ./imports/nixpkgs/nixos system $@ && {
     dir=$(pwd)
     gksudo "nix-env --profile /nix/var/nix/profiles/system --set $(readlink $dir/result)"
     gksudo "$dir/result/bin/switch-to-configuration switch"
