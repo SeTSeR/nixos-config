039097dd6a8a22d6566b6a8d3700795508d1ea3e
Sergey Makarov <setser200018@gmail.com>
2019-08-09 17:57:58 +0300

    Enable using lsp on remote machine

--- modules/users/smakarov/emacs/lsp.el
+++ modules/users/smakarov/emacs/lsp.el
@@ -8,7 +8,21 @@
   :commands lsp
   :config
   (setq lsp-prefer-flymake nil)
-  (setq lsp-enable-snippet nil))
+  (setq lsp-enable-snippet nil)
+  (lsp-register-client
+    (make-lsp-client :new-connection (lsp-tramp-connection "rls")
+                  :major-modes '(rust-mode rustic-mode)
+                  :priority -1
+                  :server-id 'rls-remote
+                  :initialization-options '((omitInitBuild . t)
+                                            (cmdRun . t))
+                  :notification-handlers (ht ("window/progress" 'lsp-clients--rust-window-progress))
+                  :action-handlers (ht ("rls.run" 'lsp-rust--rls-run))
+                  :library-folders-fn (lambda (_workspace) lsp-rust-library-directories)
+                  :initialized-fn (lambda (workspace)
+                                    (with-lsp-workspace workspace
+                                      (lsp--set-configuration
+                                       (lsp-configuration-section "rust")))))))
 
 (use-package lsp-ui
   :commands lsp-ui-mode)
