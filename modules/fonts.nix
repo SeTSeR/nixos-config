0933b9a54a542b0c63592a5fc1f9a7ad0271d355
Sergey Makarov <setser200018@gmail.com>
2019-08-24 13:53:44 +0300

    Adjust font settings

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,16 +1,26 @@
 { config, pkgs, ... }: {
-  fonts.fonts = with pkgs; [
-    dejavu_fonts
-    fira-code
-    fira-code-symbols
-    font-awesome_5
-    liberation_ttf
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    powerline-fonts
-    source-code-pro
-    symbola
-    ubuntu_font_family
-  ];
+  fonts = {
+    fontconfig = {
+      enable = true;
+      antialias = true;
+      defaultFonts = {
+        monospace = [ "Iosevka" ];
+      };
+    };
+    fonts = with pkgs; [
+      dejavu_fonts
+      fira-code
+      fira-code-symbols
+      font-awesome_5
+      iosevka
+      liberation_ttf
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      powerline-fonts
+      source-code-pro
+      symbola
+      ubuntu_font_family
+    ];
+  };
 }
