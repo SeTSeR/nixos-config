fc8c24fb0502abc8d72a5bf5c12c177885623fbd
Sergey Makarov <setser200018@gmail.com>
2019-12-22 14:31:29 +0300

    Replace deprecated parameters

--- default.nix
+++ default.nix
@@ -16,11 +16,11 @@ in
   inherit device;
 
   # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
   };
+  i18n.defaultLocale = "en_US.UTF-8";
 
   # Set your time zone.
   time.timeZone = "Europe/Moscow";
