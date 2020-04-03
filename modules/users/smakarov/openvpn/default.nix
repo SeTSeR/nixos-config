9d26ea19dcaca10a7a222c285cd0791ca8c197c8
Sergey Makarov <setser200018@gmail.com>
2020-04-03 15:27:39 +0300

    Add new OpenVPN server

--- modules/users/smakarov/openvpn/default.nix
+++ modules/users/smakarov/openvpn/default.nix
@@ -2,7 +2,16 @@
 {
   services.openvpn = {
     servers = {
-      ispras = {
+      ispras-aviator = {
+        autoStart = false;
+        config = "config ${config.openVPNConfigPath}/aviator-dep-padaryan.conf";
+        authUserPass = {
+          username = config.secrets.isprasUserName;
+          password = config.secrets.isprasPassword;
+        };
+        updateResolvConf = true;
+      };
+      ispras-bird = {
         autoStart = false;
         config = "config ${config.openVPNConfigPath}/bird.ovpn";
         authUserPass = {
@@ -15,4 +24,7 @@
   };
 
   home-manager.users.smakarov.home.file.".config/openvpn/bird.ovpn".source = ./bird.ovpn;
+  home-manager.users.smakarov.home.file.".config/openvpn/aviator-dep-padaryan.conf".source = ./aviator-dep-padaryan.conf;
+  home-manager.users.smakarov.home.file.".config/openvpn/ca-aviator.crt".source = ./ca-aviator.crt;
+  home-manager.users.smakarov.home.file.".config/openvpn/ta-aviator.key".source = ./ta-aviator.key;
 }
