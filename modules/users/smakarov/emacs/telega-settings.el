2e52f09a8ebef341cb6eb1a1e17375066236a8ee
Sergey Makarov <setser200018@gmail.com>
2020-04-21 22:33:07 +0300

    Test client-side message filtering

--- modules/users/smakarov/emacs/telega-settings.el
+++ modules/users/smakarov/emacs/telega-settings.el
@@ -3,10 +3,16 @@
 ;;; Telega.el settings
 
 ;;; Code:
+(defun telega-ignore-larry-orwell (msg &rest notused)
+  (when (= (plist-get msg :sender_user_id) 165277130)
+    (telega-msg-ignore msg)))
+
 (use-package telega
   :commands (telega)
   :defer t
+  :hook (telega-chat-pre-message . (telega-ignore-larry-orwell))
   :config
+  (telega-notifications-mode 1)
   (setq telega-proxies
         (list
          '(:server "185.86.77.210" :port 3443 :enable :false :type
