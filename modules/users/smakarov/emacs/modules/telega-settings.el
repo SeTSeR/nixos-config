00b4187dbabec5ea0a652024bc5cafc397e82715
Sergey Makarov <setser200018@gmail.com>
2020-08-09 13:04:02 +0300

    Additional settings for telega.el

--- modules/users/smakarov/emacs/modules/telega-settings.el
+++ modules/users/smakarov/emacs/modules/telega-settings.el
@@ -11,7 +11,11 @@
   :commands (telega)
   :defer t
   :bind-keymap ("C-c t" . telega-prefix-map)
+  :hook
+  ((telega-load . telega-mode-line-mode)
+   (telega-load . telega-squash-message-mode))
   :config
+  (use-package telega-mnz)
   (setq telega-proxies
         (list
          '(:server "185.86.77.210" :port 3443 :enable :false :type
