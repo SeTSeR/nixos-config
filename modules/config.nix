f051ffdc5379ee25e9be2ec2bcd5d35fb079aed1
Sergey Makarov <setser200018@gmail.com>
2020-09-05 16:44:17 +0300

    Use custom packaged emacs-direnv

--- modules/config.nix
+++ modules/config.nix
@@ -1,16 +1,5 @@
 { config, lib, pkgs, ... }:
-let overrides = self: super: rec {
-      direnv = self.melpaPackages.direnv.override {
-        src = pkgs.fetchFromGitHub {
-          owner = "wbolster";
-          repo = "emacs-direnv";
-          rev = "e547e4b658dd3ac6110663109f94b9b05bd032a2";
-          sha256 = "sha256-GGH1JbypThqVZmom3vLUGbqYdwx/8QsMU3mB/t0SKto=";
-          fetchSubmodules = true;
-        };
-      };
-    };
-in with lib; {
+with lib; {
   options.openVPNConfigPath = mkOption {
     type = types.str;
     description = "Path to OpenVPN configuration";
@@ -22,59 +11,79 @@ in with lib; {
   };
 
   config.openVPNConfigPath = "${config.users.users.smakarov.home}/.config/openvpn";
-  config.emacsPackage = ((pkgs.emacsPackagesNgGen pkgs.emacs).overrideScope' overrides).emacsWithPackages(epkgs:
-  with epkgs.melpaPackages; [
-    ace-window
-    apropospriate-theme
-    avy
-    ccls
-    company-irony
-    company-lsp
-    company-nixos-options
-    counsel
-    counsel-projectile
-    diminish
-    direnv
-    eshell-toggle
-    geiser
-    flycheck
-    flycheck-irony
-    flycheck-pkg-config
-    flycheck-rust
-    flyspell-correct
-    free-keys
-    glsl-mode
-    gnuplot
-    google-c-style
-    graphviz-dot-mode
-    gruvbox-theme
-    haskell-mode
-    irony
-    ivy
-    ivy-bibtex
-    ivy-xref
-    ivy-yasnippet
-    lsp-mode
-    lsp-ui
-    magit
-    markdown-mode
-    multitran
-    nix-mode
-    org-ref
-    pdf-tools
-    powerline
-    projectile
-    racket-mode
-    rainbow-delimiters
-    reverse-im
-    rust-mode
-    smartparens
-    quack
-    telega
-    use-package
-    visual-fill-column
-    wakatime-mode
-    yasnippet
-    yasnippet-snippets
-  ]);
+  config.emacsPackage = (pkgs.emacsPackagesNgGen pkgs.emacs).emacsWithPackages(epkgs:
+    with epkgs.melpaPackages; [
+      ace-window
+      apropospriate-theme
+      avy
+      ccls
+      company-irony
+      company-lsp
+      company-nixos-options
+      counsel
+      counsel-projectile
+      diminish
+      eshell-toggle
+      geiser
+      flycheck
+      flycheck-irony
+      flycheck-pkg-config
+      flycheck-rust
+      flyspell-correct
+      free-keys
+      glsl-mode
+      gnuplot
+      google-c-style
+      graphviz-dot-mode
+      gruvbox-theme
+      haskell-mode
+      irony
+      ivy
+      ivy-bibtex
+      ivy-xref
+      ivy-yasnippet
+      lsp-mode
+      lsp-ui
+      magit
+      markdown-mode
+      multitran
+      nix-mode
+      org-ref
+      pdf-tools
+      powerline
+      projectile
+      racket-mode
+      rainbow-delimiters
+      reverse-im
+      rust-mode
+      smartparens
+      quack
+      telega
+      use-package
+      visual-fill-column
+      wakatime-mode
+      yasnippet
+      yasnippet-snippets
+    ] ++
+    [
+      ((pkgs.emacsPackagesNgGen pkgs.emacs).melpaBuild {
+        pname = "direnv";
+        ename = "direnv";
+        version = "20200818";
+        recipe = pkgs.writeText "recipe" ''
+          (direnv
+            :fetcher github
+            :repo "wbolster/emacs-direnv"
+            :commit "e547e4b658dd3ac6110663109f94b9b05bd032a2")
+        '';
+        src = pkgs.fetchFromGitHub {
+          owner = "wbolster";
+          repo = "emacs-direnv";
+          rev = "e547e4b658dd3ac6110663109f94b9b05bd032a2";
+          sha256 = "sha256-GGH1JbypThqVZmom3vLUGbqYdwx/8QsMU3mB/t0SKto=";
+          fetchSubmodules = true;
+        };
+      })
+    ]
+  );
 }
