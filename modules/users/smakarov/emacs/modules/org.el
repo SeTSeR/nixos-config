3b32a545db32101e481cae13f95636281d6083e2
Sergey Makarov <setser200018@gmail.com>
2020-10-24 16:52:35 +0300

    Update TODO keywords

--- modules/users/smakarov/emacs/modules/org.el
+++ modules/users/smakarov/emacs/modules/org.el
@@ -42,7 +42,30 @@
   :config
   (setq org-agenda-files (list "~/org")
         org-log-done t
-        org-enforce-todo-dependencies t)
+        org-enforce-todo-dependencies t
+        org-use-fast-todo-selection t)
+
+  (setq org-todo-keywords
+        (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+                (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
+
+  (setq org-todo-keyword-faces
+        (quote (("TODO" :foreground "red" :weight bold)
+                ("NEXT" :foreground "blue" :weight bold)
+                ("DONE" :foreground "forest green" :weight bold)
+                ("WAITING" :foreground "orange" :weight bold)
+                ("HOLD" :foreground "magenta" :weight bold)
+                ("CANCELLED" :foreground "forest green" :weight bold))))
+
+  (setq org-todo-state-tags-triggers
+      (quote (("CANCELLED" ("CANCELLED" . t))
+              ("WAITING" ("WAITING" . t))
+              ("HOLD" ("WAITING") ("HOLD" . t))
+              (done ("WAITING") ("HOLD"))
+              ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
+
   (setq org-latex-listings 'minted
         org-latex-packages-alist '(("AUTO" "babel" t ("pdflatex"))
                                    ("AUTO" "polyglossia" t ("xelatex" "lualatex")))
