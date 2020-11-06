ba773c48afb2cd0edf9142c1d707239b544f1320
Sergey Makarov <setser200018@gmail.com>
2020-11-07 00:17:49 +0300

    Migrate to a 'erase your darlings'-like setup, part 1

--- default.nix
+++ default.nix
@@ -26,5 +26,5 @@ rec {
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.09"; # Did you read the comment?
+  system.stateVersion = "20.09"; # Did you read the comment?
 }
