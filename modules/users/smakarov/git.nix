dde278af92370f74b303552aa6bd3c5a53ec9a26
Sergey Makarov <setser200018@gmail.com>
2020-11-07 00:18:09 +0300

    Fix GPG key for git

--- modules/users/smakarov/git.nix
+++ modules/users/smakarov/git.nix
@@ -2,7 +2,7 @@
   home-manager.users.smakarov.programs.git = {
     enable = true;
     signing.signByDefault = true;
-    signing.key = if config.deviceSpecific.isWorkMachine then "3C0A077647B0C401F7821DE61CAD6E80CE0210A7" else "6F8A18AE4101103F3C5424B96AA23A1193B7064B";
+    signing.key = if config.deviceSpecific.isWorkMachine then "3C0A077647B0C401F7821DE61CAD6E80CE0210A7" else "96FF6722A07338C7";
     userEmail = if config.deviceSpecific.isWorkMachine then "smakarov@ispras.ru" else "setser200018@gmail.com";
     userName = "Sergey Makarov";
   };
