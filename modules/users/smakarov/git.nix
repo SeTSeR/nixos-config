9c51a8ba172b27142945c971decdc1f2378a60d3
Sergey Makarov <setser200018@gmail.com>
2020-11-22 22:46:16 +0300

    Move to GPG keys, part 2

--- modules/users/smakarov/git.nix
+++ modules/users/smakarov/git.nix
@@ -2,7 +2,7 @@
   home-manager.users.smakarov.programs.git = {
     enable = true;
     signing.signByDefault = true;
-    signing.key = if config.deviceSpecific.isWorkMachine then "3C0A077647B0C401F7821DE61CAD6E80CE0210A7" else "96FF6722A07338C7";
+    signing.key = if config.deviceSpecific.isWorkMachine then "3C0A077647B0C401F7821DE61CAD6E80CE0210A7" else "0x29ED1FC2B32F2D88";
     userEmail = if config.deviceSpecific.isWorkMachine then "smakarov@ispras.ru" else "setser200018@gmail.com";
     userName = "Sergey Makarov";
   };
