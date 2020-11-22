7bfa92c0311fb013e9a20079e10512878429e335
Sergey Makarov <setser200018@gmail.com>
2020-11-22 16:43:15 +0300

    Move stateVersion to machine-specific settings

--- default.nix
+++ default.nix
@@ -21,10 +21,4 @@ rec {
 
   # Set your time zone.
   time.timeZone = "Europe/Moscow";
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "20.09"; # Did you read the comment?
 }
--- machines/ASUS-Laptop.nix
+++ machines/ASUS-Laptop.nix
@@ -56,4 +56,10 @@
     ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "20.09"; # Did you read the comment?
 }
