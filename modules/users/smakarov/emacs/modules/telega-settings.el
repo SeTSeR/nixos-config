7e8bf9bcdf3b9b2354ff9df7f5d7223d649b9c18
Sergey Makarov <setser200018@gmail.com>
2021-05-21 23:30:50 +0300

    Fix telega settings

--- modules/users/smakarov/emacs/modules/telega-settings.el
+++ modules/users/smakarov/emacs/modules/telega-settings.el
@@ -3,28 +3,31 @@
 ;;; Telega.el settings
 
 ;;; Code:
+(use-package telega-stories
+  :after telega
+  :bind
+  (:map telega-root-mode-map
+        ("v e" . telega-view-emacs-stories))
+  :config
+  (telega-stories-mode 1))
+
+(use-package telega-dired-dwim :after telega)
+(use-package ol-telega :after telega)
+(use-package telega-mnz
+  :after telega
+  :hook ((telega-load . global-telega-mnz-mode)))
+
 (use-package telega
   :init
   (defun ignore-user-messages-by-id (id msg)
       (when (= (plist-get msg :sender_user_id) id)
         (telega-msg-ignore msg)))
   :commands (telega)
-  :defer t
   :bind-keymap ("C-c t" . telega-prefix-map)
   :hook
   ((telega-load . telega-mode-line-mode)
-   (telega-load . telega-squash-message-mode)
-   (telega-load . global-telega-mnz-mode))
+   (telega-load . telega-squash-message-mode))
   :config
-  (use-package telega-stories
-    :bind
-    (:map telega-root-mode-map
-          ("v e" . telega-view-emacs-stories))
-    :config
-    (telega-stories-mode 1))
-  (use-package telega-dired-dwim)
-  (use-package telega-mnz)
-  (use-package ol-telega)
   (setq telega-proxies
         (list
          '(:server "185.86.77.210" :port 3443 :enable :false :type
