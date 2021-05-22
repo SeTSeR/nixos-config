ca9ea503862fc45cee0e9fd5c886f2390a959d96
Sergey Makarov <setser200018@gmail.com>
2021-05-22 16:37:17 +0300

    Set up gnus

--- modules/users/smakarov/emacs/modules/mail.el
+++ modules/users/smakarov/emacs/modules/mail.el
@@ -18,6 +18,38 @@
 
 (use-package gnus
   :custom
-  (gnus-home-directory "@emacsConfigDir@"))
+  (user-mail-address "setser200018@gmail.com")
+  (gnus-nntp-server nil)
+  (gnus-select-method '(nnnil ""))
+  (gnus-secondary-select-methods
+   '((nnmaildir "cmc"
+		(directory "~/Maildir/cmc")
+		(gnus-search-engine gnus-search-notmuch
+				    (config-file "@notmuchrc@"))
+		(get-new-mail nil))
+     (nnmaildir "work"
+		(directory "~/Maildir/ispras")
+		(gnus-search-engine gnus-search-notmuch
+				    (config-file "@notmuchrc@"))
+		(get-new-mail nil))
+     (nnmaildir "personal"
+		(directory "~/Maildir/main")
+		(gnus-search-engine gnus-search-notmuch
+				    (config-file "@notmuchrc@"))
+		(get-new-mail nil))
+     (nnmaildir "microsoft"
+		(directory "~/Maildir/microsoft")
+		(gnus-search-engine gnus-search-notmuch
+				    (config-file "@notmuchrc@"))
+		(get-new-mail nil))
+     (nnmaildir "yandex"
+		(directory "~/Maildir/yandex")
+		(gnus-search-engine gnus-search-notmuch
+				    (config-file "@notmuchrc@"))
+		(get-new-mail-nil))))
+  (mm-text-html-renderer 'gnus-w3m)
+  (send-mail-function 'sendmail-send-it)
+  (message-send-mail-function 'sendmail-send-it)
+  (mml-secure-openpgp-sign-with-sender t))
 
 ;;; mail.el ends here
