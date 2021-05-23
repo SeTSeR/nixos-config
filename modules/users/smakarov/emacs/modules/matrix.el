7905a2480ff8105f45072858fcdd801857d994d8
Sergey Makarov <setser200018@gmail.com>
2021-05-23 23:22:48 +0300

    Matrix client: save token on successful login

--- modules/users/smakarov/emacs/modules/matrix.el
+++ modules/users/smakarov/emacs/modules/matrix.el
@@ -6,6 +6,7 @@
 
 (use-package matrix-client
   :quelpa (matrix-client :fetcher github :repo "alphapapa/matrix-client.el"
-                         :files (:defaults "logo.png" "matrix-client-standalone.el.sh")))
+                         :files (:defaults "logo.png" "matrix-client-standalone.el.sh"))
+  :custom (matrix-client-save-token t))
 
 ;;; matrix.el ends here
