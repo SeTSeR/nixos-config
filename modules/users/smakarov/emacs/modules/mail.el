5f57aed108764f753d683abf25355dcd02a07c94
Sergey Makarov <setser200018@gmail.com>
2021-05-23 22:55:41 +0300

    Attempt to fix mail settings

--- modules/users/smakarov/emacs/modules/mail.el
+++ modules/users/smakarov/emacs/modules/mail.el
@@ -25,30 +25,33 @@
    '((nnmaildir "cmc"
 		(directory "@maildir@/cmc")
 		(gnus-search-engine gnus-search-notmuch
-				    (config-file "@notmuchrc@"))
+				    (remove-prefix "@maildir@/cmc"))
 		(get-new-mail nil))
      (nnmaildir "work"
 		(directory "@maildir@/ispras")
 		(gnus-search-engine gnus-search-notmuch
-				    (config-file "@notmuchrc@"))
+				    (remove-prefix "@notmuchrc@/ispras"))
 		(get-new-mail nil))
      (nnmaildir "personal"
 		(directory "@maildir@/main")
 		(gnus-search-engine gnus-search-notmuch
-				    (config-file "@notmuchrc@"))
+				    (remove-prefix "@maildir@/personal"))
 		(get-new-mail nil))
      (nnmaildir "microsoft"
 		(directory "@maildir@/microsoft")
 		(gnus-search-engine gnus-search-notmuch
-				    (config-file "@notmuchrc@"))
+				    (remove-prefix "@maildir@/microsoft"))
 		(get-new-mail nil))
      (nnmaildir "yandex"
 		(directory "@maildir@/yandex")
 		(gnus-search-engine gnus-search-notmuch
-				    (config-file "@notmuchrc@"))
+				    (remove-prefix "@maildir@/yandex"))
 		(get-new-mail-nil))))
   (send-mail-function 'sendmail-send-it)
   (message-send-mail-function 'sendmail-send-it)
-  (mml-secure-openpgp-sign-with-sender t))
+  (mml-secure-openpgp-sign-with-sender t)
+  (gnus-asynchronous t)
+  (gnus-search-use-parsed-queries t)
+  (gnus-search-notmuch-config-file "@notmuchrc@"))
 
 ;;; mail.el ends here
