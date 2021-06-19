f7b40522b6003a212f2a3280bdca9065cf68f286
Sergey Makarov <setser200018@gmail.com>
2021-06-19 14:51:04 +0300

    .xsession fixes

--- modules/users/smakarov/xsession.nix
+++ modules/users/smakarov/xsession.nix
@@ -3,8 +3,11 @@
   home-manager.users.smakarov.xsession = {
     enable = true;
     initExtra = ''
+${pkgs.xorg.xhost}/bin/xhost +SI:localuser:$USER
 ${pkgs.xorg.xmodmap}/bin/xmodmap ${config.users.users.smakarov.home}/.Xmodmap
+${pkgs.xorg.xsetroot}/bin/xsetroot -cursor_name left_ptr
+
     '';
-    windowManager.command = "exec ${config.emacsPackage}/bin/emacs --fullscreen";
+    windowManager.command = "exec dbus-launch --exit-with-session ${config.emacsPackage}/bin/emacs --fullscreen";
   };
 }
