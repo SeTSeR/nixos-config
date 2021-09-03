126ee441fad7b81bf8199a46f15f8dba147c89ca
Sergey Makarov <setser200018@gmail.com>
2021-09-03 23:37:28 +0300

    Remove old OpenVPN config

--- modules/config.nix
+++ modules/config.nix
@@ -10,17 +10,11 @@ let emacsWithPackages = {
         extraPackages (override epkgs)
       );
 in {
-  options.openVPNConfigPath = mkOption {
-    type = types.str;
-    description = "Path to OpenVPN configuration";
-  };
-
   options.emacsPackage = mkOption {
     type = types.package;
     description = "Emacs package to use";
   };
 
-  config.openVPNConfigPath = "${config.users.users.smakarov.home}/.config/openvpn";
   config.emacsPackage = emacsWithPackages {
     package = pkgs.emacsGcc;
     override = epkgs: epkgs // {
@@ -64,7 +58,6 @@ in {
       irony
       lsp-mode
       lsp-ui
-      magit
       marginalia
       markdown-mode
       multitran
--- modules/users/smakarov/default.nix
+++ modules/users/smakarov/default.nix
@@ -6,7 +6,6 @@
     ./gnupg.nix
     ./i3
     ./mail.nix
-    ./openvpn.nix
     ./packages.nix
     ./services.nix
     ./smakarov.nix
