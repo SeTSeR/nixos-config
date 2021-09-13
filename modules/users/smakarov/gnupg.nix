1301312d3ad9a7c7b435aa06fa9ec212d11dc45d
Sergey Makarov <setser200018@gmail.com>
2021-09-13 10:22:59 +0300

    Set up other gnupg key

--- modules/users/smakarov/gnupg.nix
+++ modules/users/smakarov/gnupg.nix
@@ -10,7 +10,10 @@
     services.gpg-agent = {
       enable = true;
       enableSshSupport = true;
-      sshKeys = [ "9BDEE73D9C3BF46A833E71207C9F8FD1778B6008" ];
+      sshKeys = [
+        "9BDEE73D9C3BF46A833E71207C9F8FD1778B6008"
+        "E55515079A1F1D827849C252AEF4F49DB09A90FC"
+      ];
       pinentryFlavor = "qt";
       extraConfig = ''
           allow-emacs-pinentry
