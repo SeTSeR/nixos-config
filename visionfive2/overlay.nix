01db8bc69fed3cc556b26f38ea0114736e910c8e
Sergey Makarov <12733495+SeTSeR@users.noreply.github.com>
2025-10-04 15:14:00 +0300

    [visionfive2] Fix tailscale
    Signed-off-by: Sergey Makarov <12733495+SeTSeR@users.noreply.github.com>

--- visionfive2/overlay.nix
+++ visionfive2/overlay.nix
@@ -1,11 +1,16 @@
 self: super:
 let disableTests = pkg: pkg.overrideAttrs (self: super: { doCheck = false; });
 in {
-  libpng = super.libpng.overrideAttrs (oldAttrs: {
+  libpng = super.libpng.overrideAttrs (finalAttrs: prevAttrs: {
     configureFlags = [ "--disable-riscv-rvv" "--enable-apng" ];
   });
   libsecret = disableTests super.libsecret;
   ffmpeg = super.ffmpeg.override { withSdl2 = false; };
+  tailscale = super.tailscale.overrideAttrs (self: super: {
+    src = super.src.overrideAttrs (prev: {
+      patches = [ ./tailscale/tailscale-disable-bolt.patch ];
+    });
+  });
   python3 = super.python3.override {
     packageOverrides = pyself: pysuper: {
       virtualenv = pysuper.virtualenv.overridePythonAttrs (oldAttrs: {
