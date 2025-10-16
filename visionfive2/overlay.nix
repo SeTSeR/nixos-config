4ce7c5af333a1f5504797d3d15b779a6b7e26b57
Sergey Makarov <12733495+SeTSeR@users.noreply.github.com>
2025-10-17 02:36:03 +0300

    [visionfive2] Fix packages
    Signed-off-by: Sergey Makarov <12733495+SeTSeR@users.noreply.github.com>

--- visionfive2/overlay.nix
+++ visionfive2/overlay.nix
@@ -20,6 +20,22 @@ in
       });
     }
   );
+  bmake = super.bmake.overrideAttrs (self: super: {
+    env.BROKEN_TESTS = super.env.BROKEN_TESTS + " cmd-interrupt";
+  });
+  tbb_2022 = super.tbb_2022.overrideAttrs (finalAttrs: prevAttrs: {
+    patches = prevAttrs.patches ++ [
+      (super.fetchpatch {
+        url = "https://github.com/uxlfoundation/oneTBB/commit/65d46656f56200a7e89168824c4dbe4943421ff9.patch?full_index=1";
+        hash = "sha256-hhHDuvUsWSqs7AJ5smDYUP1yYZmjV2VISBeKHcFAfG4=";
+      })
+      (super.fetchpatch {
+        url = "https://github.com/uxlfoundation/oneTBB/commit/e57411968661ab1205322ba1c84fc1cd90a306c6.patch";
+        hash = "sha256-PFixW4lYqA5oy4LSwewvxgJbjVKJceRHnp8mgW9zBF0=";
+      })
+    ];
+  });
+  swtpm = disableTests super.swtpm;
   python3 = super.python3.override {
     packageOverrides = pyself: pysuper: {
       virtualenv = pysuper.virtualenv.overridePythonAttrs (oldAttrs: {
