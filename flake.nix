2e353cd393386a833c9de6b54122377bc323a40b
Sergey Makarov <12733495+SeTSeR@users.noreply.github.com>
2025-10-18 13:36:40 +0000

    [galaxy-s22] Add config
    Signed-off-by: Sergey Makarov <12733495+SeTSeR@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -4,14 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      ...
-    }@inputs:
+      nixos-hardware,
+      nix-on-droid
+    }:
     {
       nixosConfigurations = {
         main-pc = nixpkgs.lib.nixosSystem {
@@ -38,17 +43,24 @@
           system = "riscv64-linux";
           modules = [
             ./visionfive2/configuration.nix
-            inputs.nixos-hardware.nixosModules.starfive-visionfive-2
+            nixos-hardware.nixosModules.starfive-visionfive-2
           ];
           specialArgs = {
             inherit self nixpkgs;
           };
         };
       };
+
       formatter.aarch64-linux = nixpkgs.legacyPackages.aarch64-linux.nixfmt-tree;
+
       packages.riscv64-linux = import nixpkgs {
         system = "riscv64-linux";
         overlays = [ (import ./visionfive2/overlay.nix) ];
+      };
+
+      nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+        pkgs = import nixpkgs { system = "aarch64-linux"; };
+        modules = [ ./galaxy-s22/configuration.nix ];
       };
     };
 }
---
+++ galaxy-s22/configuration.nix
@@ -0,0 +1,52 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    # User-facing stuff that you really really want to have
+    vim # or some other editor, e.g. nano or neovim
+
+    # Some common stuff that people expect to have
+    procps
+    killall
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+    openssh
+    mosh
+    git
+    (emacs.pkgs.withPackages (epkgs: with epkgs; [
+      org
+      melpaPackages.telega
+      tree-sitter
+      vterm
+      nix-mode
+    ]))
+  ];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "24.05";
+
+  # Set up nix for flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+    # Set your time zone
+  #time.timeZone = "Europe/Berlin";
+}
